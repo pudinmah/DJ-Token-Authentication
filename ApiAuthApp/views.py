3afbdea4d8daff45cde9c8c1686492ce33d611f5
Mahpudin <pudinmah69@gmail.com>
2024-02-18 23:24:20 +0700

    Creating Necessary Endpoints

--- ApiAuthApp/views.py
+++ ApiAuthApp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(["POST"])
+def signup(request):
+
+    return Response({"message":"sign up page"})
+
+@api_view(["POST"])
+def login(request):
+
+    return Response({"message":"login page"})
+
+@api_view(["GET"])
+def TestView(request):
+
+    return Response({"message":"Test View page"})
+
+@api_view(["POST"])
+def logout(request):
+
+    return Response({"message": "logout page"})
